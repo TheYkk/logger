1bdc68a553c6b200a16cc063a4dd2d47932ca834
Kaan Karakaya <yusufkaan142@gmail.com>
2020-03-31 18:42:09 +0300

    Added node info and container info
    Signed-off-by: Kaan Karakaya <yusufkaan142@gmail.com>

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+# Install npm packages
+FROM node:12-alpine as builder
+WORKDIR /usr/src/app
+COPY package.json .
+RUN yarn install
+
+# Push js files
+FROM node:12-alpine
+WORKDIR /usr/src/app
+COPY --from=builder /usr/src/app/node_modules /usr/src/app/node_modules
+COPY ./src ./src
+CMD node src/index.js
--- src/watch.js
+++ src/watch.js
@@ -1,16 +1,36 @@
 const chokidar = require('chokidar');
+
 const fs = require('fs');
+
 const {insertLog} = require('./db');
+
+const os = require('os');
 
 const file_stats = {};
 
+const fileName = (path) => {
+  return path.split('\\').pop().split('/').pop().slice(0, -4);
+};
+
+const nodeInfo = () => {
+  const hostname = fs.readFileSync('/etc/hostname', 'utf8').split(/\n/g)[0];
+  return {
+    uptime: os.uptime(),
+    cpus: os.cpus().length,
+    arch: os.arch(),
+    platform: os.platform(),
+    hostname,
+  };
+};
+
 const watch = (main_path) => {
+  console.log(`Watching ${main_path}`);
   chokidar.watch(main_path).on('all', (event, path, stats) => {
     if (event == 'add') {
       file_stats[path] = stats.size;
     }
     if (event == 'change') {
-      fs.readFile(path, function (err, data) {
+      fs.readFile(path, (err, data) => {
         if (err) throw err;
 
         // ? Get file changed part
@@ -26,10 +46,19 @@ const watch = (main_path) => {
         // ? Remove new line
         const final = getjson.replace(/(\r\n|\n|\r)/gm, '');
 
-        console.log(final);
+        // console.log(final);
+        let logum;
+        try {
+          logum = JSON.parse(final);
+        } catch (_err) {
+          logum = final;
+        }
 
+        const fileInfo = fileName(path);
+
+        const [podname, namespace, cotainer] = fileInfo.split('_');
         // ? Insert log to mongodb
-        insertLog(final);
+        insertLog({log: logum, podname, namespace, cotainer, node: nodeInfo()});
 
         // ? Change file size
         file_stats[path] = stats.size;
