e035724f186f877d9067638f931d3b366cd75f5e
Kaan Karakaya <yusufkaan142@gmail.com>
2020-10-02 09:40:23 +0300

    Save path without extension
    Signed-off-by: Kaan Karakaya <yusufkaan142@gmail.com>

--- src/watch.js
+++ src/watch.js
@@ -52,14 +52,15 @@ const watch = (main_path) => {
 
   console.log(`Watching ${main_path}`);
   chokidar.watch(main_path).on('all', (event, path, stats) => {
-    if (path.indexOf('theykk-logger') >= 0) return;
+    const file = path.split('.').slice(0, -1).join('.');
+    if (file.indexOf('theykk-logger') >= 0) return;
 
     if (event == 'add') {
-      console.log(`Added path ${path}`);
+      console.log(`Added path ${file}`);
 
       //* If file new save to object and mongodb
-      if (!file_stats[path]) {
-        file_stats[path] = stats.size;
+      if (!file_stats[file]) {
+        file_stats[file] = stats.size;
         savePaths(file_stats);
       }
     }
@@ -69,7 +70,7 @@ const watch = (main_path) => {
 
         // ? Get file changed part
         const changed_line = data
-          .slice(file_stats[path], stats.size)
+          .slice(file_stats[file], stats.size)
           .toString();
 
         // ? Parse new lines
@@ -79,34 +80,35 @@ const watch = (main_path) => {
           if (line.length < 10) return;
           //* Example log: 2020-09-08T11:34:47.881738667+03:00 stdout F [2020/09/08 08:34:47] [logger.go:490] mapping path "/dev/null" => file system "/dev/null"
           const split_file = line.split(' ');
+          if (new Date(split_file[0]).toISOString()) {
+            // ? Remove timestampts etc.
+            const getjson = split_file.slice(3, split_file.length).join(' ');
 
-          // ? Remove timestampts etc.
-          const getjson = split_file.slice(3, split_file.length).join(' ');
+            const logum = parse(getjson);
+            const fileInfo = fileName(path);
 
-          const logum = parse(getjson);
-          const fileInfo = fileName(path);
+            const [pod, namespace, containerRaw] = fileInfo.split('_');
+            const conta = containerRaw.split('-');
+            // ? Insert log to mongodb
+            console.log(`Pod logs inserted ${pod}`);
 
-          const [pod, namespace, containerRaw] = fileInfo.split('_');
-          const conta = containerRaw.split('-');
-          // ? Insert log to mongodb
-          console.log(`Pod logs inserted ${pod}`);
-
-          insertLog({
-            log: logum,
-            pod,
-            namespace,
-            container: conta.slice(0, conta.length - 1).join('-'),
-            containerId: conta.slice(conta.length - 1)[0].substring(0, 12),
-            node: nodeInfo(),
-            time: new Date().toISOString(),
-            logTime: new Date(split_file[0]).toISOString(),
-            writer: split_file[1],
-            tags: 'k8s',
-          });
+            insertLog({
+              log: logum,
+              pod,
+              namespace,
+              container: conta.slice(0, conta.length - 1).join('-'),
+              containerId: conta.slice(conta.length - 1)[0].substring(0, 12),
+              node: nodeInfo(),
+              time: new Date().toISOString(),
+              logTime: new Date(split_file[0]).toISOString(),
+              writer: split_file[1],
+              tags: 'k8s',
+            });
+          }
         });
 
         // ? Change file size
-        file_stats[path] = stats.size;
+        file_stats[file] = stats.size;
         savePaths(file_stats);
       });
     }
